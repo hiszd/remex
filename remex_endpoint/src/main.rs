ea4d5e01d98a822996b6e88808f21f82cc7befed
Zion Koyl <hiszd1@gmail.com>
2024-07-16 13:51:40 -0400

    WIP

--- remex_endpoint/src/main.rs
+++ remex_endpoint/src/main.rs
@@ -1,37 +1,93 @@
 //ENDPOINT
 use remex_core::{Message, Packet};
-use std::io::{Read, Write};
-use std::net::TcpStream;
+use tokio::net::TcpStream;
 
 const ADDRESS: &str = "127.0.0.1:4269";
 
 const SECRET: &str = "tZs3U%hqY^o$&*y%4HcF8&RyAKevUbZnkTsrjCzPGxfare3Yn9c7shVZETfPDPUc8xR%N38a!TL%2$WbkFhZqmH#jvw&d3^mryPD8Y8TqHoJHwyKSTJeQB7vK7QkW#&B";
 
-fn main() -> std::io::Result<()> {
-    let mut stream = TcpStream::connect(ADDRESS)?;
+#[tokio::main]
+async fn main() {
+  tokio::spawn(async move {
+    let stream = TcpStream::connect(ADDRESS).await.unwrap();
+    let secret = Message::new(SECRET.to_string());
+    process(&stream, secret).await;
+  })
+  .await
+  .unwrap();
+}
 
+async fn process(socket: &TcpStream, secret: Message) {
+  await_secret(socket, secret).await;
+  println!("exited loop");
+  println!("secret received and verified");
+  await_messages(socket).await;
+}
+
+async fn await_messages(socket: &TcpStream) {
+  loop {
     let mut buf = [0; 128];
-
-    println!("reading");
     let mut packets = Vec::new();
     loop {
-        stream.read_exact(&mut buf).unwrap();
-        let packet: Packet = buf.into();
-        println!("got packet: {:?}", packet);
-        packets.push(packet.clone());
-        buf = [0; 128];
-        if packet.number == packet.total {
-            break;
+      socket.readable().await.unwrap();
+      match socket.try_read(&mut buf) {
+        Ok(x) => {
+          if x != 0 {
+            let packet: Packet = buf.into();
+            // println!("got packet: {:?}", packet);
+            packets.push(packet.clone());
+            buf = [0; 128];
+            if packet.number == packet.total {
+              println!("got all packets");
+              break;
+            }
+          }
         }
-    }
-    println!("exited loop");
-
-    let str = std::str::from_utf8(&buf).unwrap();
-    if str != SECRET {
-        println!("Wrong secret: {}", str);
-        return Ok(());
+        Err(_) => continue,
+      }
     }
 
-    stream.write(&[1])?;
-    Ok(())
+    // packets.iter().for_each(|x| println!("{}, {}", x.number, x.total));
+    let received = Message::from(packets);
+    println!("got {:?}", received.get_msg());
+  }
+}
+
+async fn await_secret(socket: &TcpStream, secret: Message) {
+  {
+    let mut buf = [0; 128];
+    let mut packets = Vec::new();
+    loop {
+      socket.readable().await.unwrap();
+      socket.try_read(&mut buf).unwrap();
+      let packet: Packet = buf.into();
+      packets.push(packet.clone());
+      buf = [0; 128];
+      if packet.number == packet.total {
+        break;
+      }
+    }
+    let receivedsecret = Message::from(packets);
+    println!("got secret: {:?}", receivedsecret.get_msg());
+    assert_eq!(secret.get_msg(), receivedsecret.get_msg());
+  }
+
+  loop {
+    socket.writable().await.unwrap();
+
+    let mut sent_packets = 0;
+
+    for packet in secret.get_packets().into_iter() {
+      match socket.try_write(&packet.clone().to_vec()) {
+        Ok(_) => {
+          sent_packets = sent_packets + 1;
+        }
+        Err(e) => println!("failed to send secret: {:?}", e),
+      }
+    }
+
+    if sent_packets == secret.get_packets().len() as u8 {
+      break;
+    }
+  }
 }
--- remex_server/src/main.rs
+++ remex_server/src/main.rs
@@ -1,6 +1,7 @@
 //SERVER
-use remex_core::Message;
-use tokio::io::AsyncReadExt;
+use remex_core::{Message, Packet};
+use tokio::fs::{File, OpenOptions};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 
 const ADDRESS: &str = "127.0.0.1:4269";
@@ -11,42 +12,77 @@ const SECRET: &str = "tZs3U%hqY^o$&*y%4HcF8&RyAKevUbZnkTsrjCzPGxfare3Yn9c7shVZET
 
 #[tokio::main]
 async fn main() {
-    let listener = TcpListener::bind(ADDRESS).await.unwrap();
-    println!("listening on {}...", ADDRESS);
+  let listener = TcpListener::bind(ADDRESS).await.unwrap();
+  println!("listening on {}...", ADDRESS);
 
-    loop {
-        let (stream, _) = listener.accept().await.unwrap();
-        tokio::spawn(async move {
-            let secret = Message::new(SECRET.to_string());
-            process(stream, secret).await;
-        });
-    }
+  loop {
+    let (stream, _) = listener.accept().await.unwrap();
+    tokio::spawn(async move {
+      let secret = Message::new(SECRET.to_string());
+      process(stream, secret).await;
+    });
+  }
 }
 
-async fn process(mut socket: TcpStream, secret: Message) {
-    println!("streaming");
-    loop {
-        socket.writable().await.unwrap();
+async fn process(socket: TcpStream, secret: Message) {
+  await_secret(&socket, secret).await;
+  println!("secret received and verified");
+  write_message(&socket, "hello".to_string()).await;
+  write_message(&socket, "zion".to_string()).await;
+}
 
-        let mut sent_packets = 0;
+async fn write_message(socket: &TcpStream, mg: String) {
+  let msg = Message::new(mg);
+  for packet in msg.get_packets().into_iter() {
+    socket.writable().await.unwrap();
+    match socket.try_write(&packet.clone().to_vec()) {
+      Ok(_) => unsafe {
+        MSG_SENT.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+        println!("sent message {}", MSG_SENT.load(std::sync::atomic::Ordering::Relaxed));
+      },
+      Err(e) => println!("failed to send message: {:?}", e),
+    }
+  }
+}
 
-        for packet in secret.packets.clone().into_iter() {
-            match socket.try_write(&packet.clone().to_vec()) {
-                Ok(_) => {
-                    sent_packets = sent_packets + 1;
-                    println!("sent {} packets", sent_packets);
-                }
-                Err(e) => println!("failed to send secret: {:?}", e),
-            }
+async fn await_secret(socket: &TcpStream, secret: Message) {
+  loop {
+    socket.writable().await.unwrap();
+
+    let mut sent_packets = 0;
+
+    for packet in secret.get_packets().into_iter() {
+      match socket.try_write(&packet.clone().to_vec()) {
+        Ok(_) => {
+          sent_packets = sent_packets + 1;
         }
-
-        if sent_packets == secret.packets.len() as u8 {
-            break;
-        }
+        Err(e) => println!("failed to send secret: {:?}", e),
+      }
     }
 
-    let mut read_buf: Vec<u8> = Vec::new();
+    if sent_packets == secret.get_packets().len() as u8 {
+      break;
+    } else {
+      println!("failed to send all packets");
+    }
+  }
 
-    socket.read_to_end(&mut read_buf).await.unwrap();
-    println!("{:?}", read_buf);
+  let mut read_buf = [0; 128];
+
+  let mut packets = Vec::new();
+  loop {
+    socket.readable().await.unwrap();
+    socket.try_read(&mut read_buf).unwrap();
+
+    let packet: Packet = read_buf.into();
+    // println!("got packet: {:?}", packet);
+    packets.push(packet.clone());
+    read_buf = [0; 128];
+    if packet.number == packet.total {
+      break;
+    }
+  }
+  let receivedsecret = Message::from(packets);
+  println!("got secret: {:?}", receivedsecret.get_msg());
+  assert_eq!(secret.get_msg(), receivedsecret.get_msg());
 }
